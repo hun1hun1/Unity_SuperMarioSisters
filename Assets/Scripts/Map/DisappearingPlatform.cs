a414db5fbd381f96c4233ea90edee056a1e83cc3
Jihoon Choi <aa09302003@gmail.com>
2026-08-06 17:44:52 +0900

    add MovingPlatform.cs and DisappearingPlatform.cs

---
+++ Assets/Scripts/Map/DisappearingPlatform.cs
@@ -0,0 +1,78 @@
+using UnityEngine;
+
+public class DisappearingPlatform : MonoBehaviour
+{
+    public float visibleTime = 2.0f;
+    public float hiddenTime = 1.0f;
+
+    public SpriteRenderer platformRenderer;
+    public Collider2D platformCollider;
+
+    private float timer = 0.0f;
+    private bool isVisible = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time.deltaTime : 이전 프레임과 현재 프레임 사이의 시간 간격 -> 초 단위.
+        timer += Time.deltaTime;
+
+        // 발판을 켜기/끄기 처리.
+        if (isVisible == true)
+        {
+            CheckVisibleTime();
+        }
+        else
+        {
+            if (timer >= hiddenTime * 0.3)
+            {
+                platformRenderer.color = Color.yellow;
+            }
+            CheckHiddenTime();
+        }
+    }
+
+    void CheckVisibleTime()
+    {
+        if (timer >= visibleTime)
+        {
+            SetPlatformVisible(false);
+            timer = 0.0f;
+        }
+    }
+
+    void CheckHiddenTime()
+    {
+        if (timer >= hiddenTime)
+        {
+            SetPlatformVisible(true);
+            platformRenderer.color = Color.white;
+            timer = 0.0f;
+        }
+    }
+
+    void SetPlatformVisible(bool value)
+    {
+        isVisible = value;
+        platformRenderer.enabled = value;
+        platformCollider.enabled = value;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 게임 오브젝트의 Tag가 인자로 전달한 문자열과 일치하는지 체크하는 함수.
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            // SetParent : 인자로 전달한 트랜스폼을 부모로 설정하는 함수.
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
---
+++ Assets/Scripts/Map/MovingPlatform.cs
@@ -0,0 +1,127 @@
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float moveSpeed = 2.0f;
+    public float moveDistance = 3.0f;
+    public bool moveX = true;
+    public bool moveY = false;
+    public float stopTime = 2.0f;
+    public bool onPlayerMove = false;
+
+    private Vector3 startPosition;
+    private float moveDirection = 1.0f;
+    private float timer = 0.0f;
+    private bool isOnPlayer = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = transform.position;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onPlayerMove == true)
+        {
+            if (isOnPlayer == true)
+            {
+                if (moveX == true)
+                {
+                    // 발판 이동 처리.
+                    MovePlatformX();
+                    // 방향 전환 처리.
+                    CheckTurnPointX();
+                }
+
+                if (moveY == true)
+                {
+                    MovePlatformY();
+                    CheckTurnPointY();
+                }
+            }
+        }
+        else
+        {
+            if (moveX == true)
+            {
+                // 발판 이동 처리.
+                MovePlatformX();
+                // 방향 전환 처리.
+                CheckTurnPointX();
+            }
+
+            if (moveY == true)
+            {
+                MovePlatformY();
+                CheckTurnPointY();
+            }
+        }
+    }
+
+    void MovePlatformX()
+    {
+        Vector3 moveAmount = Vector3.right * moveDirection * moveSpeed * Time.deltaTime;
+
+        transform.position = transform.position + moveAmount;
+    }
+
+    void CheckTurnPointX()
+    {
+        float distanceFromStart = transform.position.x - startPosition.x;
+
+        if (distanceFromStart > moveDistance)
+        {
+            moveDirection = -1.0f;
+
+        }
+        else if (distanceFromStart < -moveDistance)
+        {
+            moveDirection = 1.0f;
+        }
+    }
+
+    void MovePlatformY()
+    {
+        Vector3 moveAmount = Vector3.up * moveDirection * moveSpeed * Time.deltaTime;
+
+        transform.position = transform.position + moveAmount;
+    }
+
+    void CheckTurnPointY()
+    {
+        float distanceFromStart = transform.position.y - startPosition.y;
+
+        if (distanceFromStart > moveDistance)
+        {
+            moveDirection = -1.0f;
+
+        }
+        else if (distanceFromStart < -moveDistance)
+        {
+            moveDirection = 1.0f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // 게임 오브젝트의 Tag가 인자로 전달한 문자열과 일치하는지 체크하는 함수.
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            // SetParent : 인자로 전달한 트랜스폼을 부모로 설정하는 함수.
+            collision.gameObject.transform.SetParent(transform);
+            isOnPlayer = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") == true)
+        {
+            collision.gameObject.transform.SetParent(null);
+            isOnPlayer = false;
+        }
+    }
+}
